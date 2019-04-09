65bd52faa85e4d77f30bad2922e708dde58db196
guodd369 <guodd369@163.com>
2019-04-09 17:45:19 +0800

    基础知识

--- base-01/src/main/java/ch05_containers/arrays/ArraysMainTest01.java
+++ base-01/src/main/java/ch05_containers/arrays/ArraysMainTest01.java
@@ -14,7 +14,7 @@ import java.util.List;
  */
 public class ArraysMainTest01 {
     public static void main(String[] args) {
-        List<String> strings = Arrays.asList("1", "2", "3");
-
+        List<String> strings = Arrays.asList("a", "b", "c");
+        // removeIf
     }
 }
