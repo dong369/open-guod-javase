ff3d2071d65cb44d7c7792674115ac86b8d308bf
guodd <guodd369@163.com>
2019-05-23 21:00:57 +0800

    基础

---
+++ base-09/base-09-01/src/main/bat/ch02_data/path.cmd
@@ -0,0 +1,8 @@
+rem 跳转到kettle工作目录下
+d:
+cd D:/
+set pathVal = D:\
+chcp 65001
+rem 文档注释
+echo %pathVal%
+pause
--- base-09/base-09-01/src/main/bat/ch02_data/rem.cmd
+++ base-09/base-09-01/src/main/bat/ch02_data/rem.cmd
@@ -1,6 +1,3 @@
-rem 跳转到kettle工作目录下
-d:
-cd D:/kettle_3.2_xgn/distrib
 @echo off
 chcp 65001
 rem 文档注释
